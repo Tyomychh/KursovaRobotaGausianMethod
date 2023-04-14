b5d0ba1be47e959f4484c580a7f4570eb7f7fda2
Tyomichh <workemailaertemlozenko159@gmail.com>
2023-04-14 12:18:57 +0300

    first commit

---
+++ KursovaRobota/KursovaRobota/JordanGausClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursovaRobota
+{
+    public class JordanGausClass
+    {
+        private double[,] matrix;
+        private double[] constants;
+
+        public JordanGausClass(double[,] coefficients, double[] constants)
+        {
+            int n = constants.Length;
+            matrix = new double[n, n];
+            this.constants = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = coefficients[i, j];
+                }
+                this.constants[i] = constants[i];
+            }
+        }
+
+        // Вирішення методом Жордана-Гауса
+        public void JordanGauss()
+        {
+            int n = constants.Length;
+
+            for (int k = 0; k < n; k++)
+            {
+                double div = matrix[k, k];
+                for (int j = k; j < n; j++)
+                {
+                    matrix[k, j] /= div;
+                }
+                constants[k] /= div;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (i != k)
+                    {
+                        double mult = matrix[i, k];
+                        for (int j = k; j < n; j++)
+                        {
+                            matrix[i, j] -= mult * matrix[k, j];
+                        }
+                        constants[i] -= mult * constants[k];
+                    }
+                }
+            }
+
+            // Виводимо результати
+            Console.WriteLine("Вирiшена система:\n");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"x{i + 1} = {constants[i]}");
+            }
+        }
+    }
+}
---
+++ KursovaRobota/KursovaRobota/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace KursovaRobota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть розмiрнiсть матрицi:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n + 1 > 11 || n < 3)
+            {
+                Console.WriteLine("\nВведiть нормальну розмiрнiсть, вона має бути не бiльше 11та не менше 3\n");
+                return;
+            }
+
+            double[,] coefficients = new double[n, n];
+
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Введiть значення елемента Рядок[{i}],Стовбець[{j}]: ");
+                    coefficients[i, j] = double.Parse(Console.ReadLine());
+                    
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введiть вектор сталих значень:\n");
+            double[] constants = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{i + 1} Стале значення: ");
+                constants[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            JordanGausClass solver = new JordanGausClass(coefficients, constants);
+            solver.JordanGauss();
+
+            Console.ReadLine();
+        }
+    }
+}
