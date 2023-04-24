781045f606106f06524eca4b032f131d05f97bb8
Tyomichh <workemailaertemlozenko159@gmail.com>
2023-04-24 22:37:21 +0300

    Add AuditClass, and reworked the program

---
+++ KursovaRobota/KursovaRobota/AuditClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursovaRobota
+{
+    public class AuditClass
+    {
+        private int k;
+        private bool question;
+        public double[,] coefficients;
+        public double[] constants;
+        public AuditClass(int n, bool q)
+        {
+            k = n;
+            question = q;
+            coefficients = new double[k, k];
+            constants = new double[k];
+        }
+
+        // Перевірка на коректність введеного числа
+        public void Poverka()
+        {
+            while (!question)
+            {
+                Console.WriteLine("\nНекоректно введенi данi, спробуйте заново та введiть число\n");
+                question = int.TryParse(Console.ReadLine(), out k);
+            }
+        }
+
+        // Введення та перевірка матриці на коректність введеного числа
+        public void VvodMatrici()
+        {
+            coefficients = new double[k, k];
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    Console.Write($"Введiть значення елемента Рядок[{i}],Стовбець[{j}]: ");
+
+                    string inp = Console.ReadLine();
+
+                    if (double.TryParse(inp, out double value))
+                    {
+                        coefficients[i, j] = value;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("\nНекоректно введенi даннi. Будь ласка, введiть число\n");
+                        j--;
+                    }
+                }
+            }
+        }
+
+        // Введення та перевірка вектора на коректність введеного числа
+        public void VvodVektora()
+        {
+            Console.WriteLine("Введiть вектор сталих значень:\n");
+            constants = new double[k];
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write($"{i + 1} Стале значення: ");
+
+                string inp = Console.ReadLine();
+
+                if (double.TryParse(inp, out double value))
+                {
+                    constants[i] = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("\nНекоректно введенi даннi. Будь ласка, введiть число\n");
+                    i--;
+                }
+            }
+        }
+
+        public void VivodVsego()
+        {
+            Console.WriteLine();
+
+            JordanGausClass solver = new JordanGausClass(coefficients, constants);
+            solver.JordanGaussMethodSolution();
+        }
+    }
+}
--- KursovaRobota/KursovaRobota/Program.cs
+++ KursovaRobota/KursovaRobota/Program.cs
@@ -6,67 +6,16 @@ namespace KursovaRobota
     {
         static void Main(string[] args)
         {
+            int chislo;
             Console.WriteLine("Введiть розмiрнiсть матрицi:");
-            int n;
-            bool isNumeric = int.TryParse(Console.ReadLine(), out n);
+            bool isNumeric = int.TryParse(Console.ReadLine(), out chislo);
 
-            if (!isNumeric)
-            {
-                Console.WriteLine("\nНекоректно введенi данi, спробуйте заново та введiть число\n");
-                return;
-            }
+            AuditClass perevirkaprog = new AuditClass(chislo, isNumeric);
+            perevirkaprog.Poverka();
+            perevirkaprog.VvodMatrici();
+            perevirkaprog.VvodVektora();
+            perevirkaprog.VivodVsego();
 
-            if (n + 1 > 11 || n < 3)
-            {
-                Console.WriteLine("\nСпробуйте заново, та введiть нормальну розмiрнiсть, вона має бути не бiльше 11 та не менше 3\n");
-                return;
-            }
-
-            double[,] coefficients = new double[n, n];
-
-            Console.WriteLine();
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write($"Введiть значення елемента Рядок[{i}],Стовбець[{j}]: ");
-                    string inp = Console.ReadLine();
-                    if (double.TryParse(inp, out double value))
-                    {
-                        coefficients[i, j] = value;
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nНекоректно введенi даннi. Будь ласка, введiть число\n");
-                        j--;
-                    }
-                }
-            }
-
-            Console.WriteLine();
-
-            Console.WriteLine("Введiть вектор сталих значень:\n");
-            double[] constants = new double[n];
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"{i + 1} Стале значення: ");
-                string inp = Console.ReadLine();
-                if (double.TryParse(inp, out double value))
-                {
-                    constants[i] = value;
-                }
-                else
-                {
-                    Console.WriteLine("\nНекоректно введенi даннi. Будь ласка, введiть число\n");
-                    i--;
-                }
-            }
-
-            Console.WriteLine();
-
-            JordanGausClass solver = new JordanGausClass(coefficients, constants);
-            solver.JordanGaussMethodSolution();
 
             Console.ReadLine();
         }
